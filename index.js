e4363cce777d0675b6ce801e1e86b3e3e184e10b
AndrewX <88537554+AWildAndrewX@users.noreply.github.com>
2021-09-13 12:40:48 +0700

    Created index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+const db = require("quick.db");
+const ms = require("ms");
+const { default_prefix } = require("./config.json");
+const emote = require("./config/emotes.json");
+const discord = require('discord.js');
+require('discord-reply');
+const client = new discord.Client();
 
+client.commands = new discord.Collection();
+client.aliases = new discord.Collection();
+
+["command", "events"].forEach(handler => {
+   require(`./handlers/${handler}`)(client);
+});
+
+//Sets Bot Status
+client.on("ready", () => {
+    client.user.setActivity(`${default_prefix}help | Made by AndrewX`, { type: "WATCHING"});
+});
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("ug");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at Port: ${port}`);
+});
+
+client.login(process.env.TOKEN);
